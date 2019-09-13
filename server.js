7734ce3d604392efa2b57efd0c608b65a144a0e0
yurika sakae <yurika.sun@gmail.com>
2019-09-13 08:48:11 -0700

    component folders created, router set up

--- server.js
+++ server.js
@@ -7,4 +7,13 @@ server.get("/", (req, res) => {
   res.send("API is up");
 });
 
+const projectRouter = require("./project/project-router.js");
+server.use("/projects", projectRouter);
+
+const resourcesRouter = require("./resource/resource-router.js");
+server.use("/resources", projectRouter);
+
+const taskRouter = require("./task/task-router.js");
+server.use("/tasks", taskRouter);
+
 module.exports = server;
