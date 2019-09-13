528348bcc96e793fe8ce088b9513eca19ab1347f
yurika sakae <yurika.sun@gmail.com>
2019-09-13 16:13:11 -0700

    fix for boolen 0 to false

--- project/project-router.js
+++ project/project-router.js
@@ -2,7 +2,17 @@ const router = require("express").Router();
 const Projects = require("./projects-model.js");
 
 router.get("/", (req, res) => {
-  Projects.get().then(projects => res.status(200).json(projects));
+  Projects.get().then(projects => {
+    new_pro = projects.map(project => {
+      if (project["completed"] === 0) {
+        project["completed"] = false;
+      } else {
+        project["completed"] = true;
+      }
+      return project;
+    });
+    res.status(200).json(new_pro);
+  });
 });
 
 router.post("/", (req, res) => {
